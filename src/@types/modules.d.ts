5d39e2afd5eec54a92d2cb88a2e4bcd026c81b40
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-10-18 21:02:43 -0300

    chore: setup env modules type

---
+++ src/@types/modules.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    NLWHEAT_GITHUB_CLIENT_SECRET: 'string';
+    NLWHEAT_GITHUB_CLIENT_ID: 'string';
+  }
+}
