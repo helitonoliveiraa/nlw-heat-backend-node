b27ee76462af72afc618841fdf7bb3d1729a39c4
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-10-20 18:07:56 -0300

    feat: integrate with socke.io

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,41 @@
+import 'dotenv/config';
 import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+import { Server } from 'socket.io';
+
+import { router } from './routes';
 
 const app = express();
+app.use(cors());
 
-app.listen(4000, () => console.log('🚀 Server is running on port 40000'));
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', socket => {
+  console.log(`User connected on socket ${socket.id}`);
+});
+
+app.use(express.json());
+
+app.use(router);
+
+app.get('/github', (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.NLWHEAT_GITHUB_CLIENT_ID}`,
+  );
+});
+
+app.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
+
+  response.json(code);
+});
+
+export { serverHttp, io };
